8e7cc968f2bd280a40f701fe68135913ba3f2e88
Sudarshan Bhattacharjee <sbhattacharjee@etouch.net>
2013-12-27 01:25:40 +0530

    logo update

--- src/main/java/com/systems/dhruva/web/RemoteDesktopViewController.java
+++ src/main/java/com/systems/dhruva/web/RemoteDesktopViewController.java
@@ -4,10 +4,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.roo.addon.json.RooJson;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.systems.dhruva.modal.Client;
 
 @RequestMapping("/remoteDesktopView/**")
 @Controller
@@ -16,6 +19,7 @@ public class RemoteDesktopViewController {
 
     @RequestMapping(method = RequestMethod.POST, value = "{id}")
     public void post(@PathVariable Long id, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
+    	System.out.println("*******************************************************************");
     }
 
     @RequestMapping
@@ -23,3 +27,5 @@ public class RemoteDesktopViewController {
         return "remoteDesktopView";
     }
 }
+
+
